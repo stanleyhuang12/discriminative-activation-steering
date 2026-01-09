5c22bfa1e08973b5392bd39a618ad06449a3e4a4
Stanley Huang <stanleyh201k@gmail.com>
2026-01-08 21:24:55 -0500

    ioi-patching-trial-error

--- probe_based_gradient_attribution/activation.py
+++ probe_based_gradient_attribution/activation.py
@@ -1,9 +1,9 @@
 
 from nnsight import LanguageModel
+nnsight
+model = LanguageModel("openai-community/gpt2")
 
-
-model = LanguageModel("openai-community/gpt2", device_map="auto")
-
+model = LanguageModel("openai-community/gpt2", device_map=None)
 import nnsight
 from nnsight import CONFIG
 
@@ -63,167 +63,54 @@ N_LAYERS = len(model.transformer.h)
 clean_hs = []
 
 # Clean run
-with model.trace() as tracer:
-    with tracer.invoke(clean_prompt) as invoker:
-        print(invoker.inputs[0]['input_ids'])
-        clean_tokens = invoker.inputs[0]['input_ids'][0]
-
-        # Get hidden states of all layers in the network.
-        # We index the output at 0 because it's a tuple where the first index is the hidden state.
-        for layer_idx in range(N_LAYERS):
-            clean_hs.append(model.transformer.h[layer_idx].output[0].save())
-
-        # Get logits from the lm_head.
-        clean_logits = model.lm_head.output
-
-        # Calculate the difference between the correct answer and incorrect answer for the clean run and save it.
-        clean_logit_diff = (
-            clean_logits[0, -1, correct_index] - clean_logits[0, -1, incorrect_index]
-        ).save()
-"""
-My test to see what the model response is 
-"""
-    
-with model.trace(corrupted_prompt) as tracer: 
-    tok_index = model.lm_head.output[0].argmax().save()
-    model.tokenizer.decode(tok_index.item())
-    
-"the"
-
-print(corrupted_logits_ls[0].size()) # batch (only 1), token, logit space (what is the model's best guess)
-corrupted_logit_diff
-print("tensor(-2.2725, grad_fn=<SubBackward0>)")
-
-
-## Patching intervention 
-
-ioi_patching_results = []
-
-"""
-For every layer, we have a dimension of [batch_idx, seq_len, hidden_state]
-For every layer, we want to index the batch, go through each seq_len and patch that hidden state with the clean one 
-Note that we stored the clean hidden states in clean_hs [batch, seq_len, hidden_state]
-We patch it by just setting those values
-"""
-
-clean_hs[0][:, 0, :]
-
-idx = []
-
-idx[0].size()
-
-ioi_patching_results = []
-
-# Iterate through all the layers
-for layer_idx in range(len(model.transformer.h)):
-    _ioi_patching_results = []
-
-    # Iterate through all tokens
-    for token_idx in range(len(clean_tokens)):
-        # Patching corrupted run at given layer and token
-        with model.trace(corrupted_prompt) as tracer:
-            # Apply the patch from the clean hidden states to the corrupted hidden states.
-            model.transformer.h[layer_idx].output[0][:, token_idx] = clean_hs[layer_idx][:,token_idx,:]
-
-            patched_logits = model.lm_head.output
-
-            patched_logit_diff = (
-                patched_logits[0, -1, correct_index]
-                - patched_logits[0, -1, incorrect_index]
-            )
-
-            # Calculate the improvement in the correct token after patching.
-            patched_result = (patched_logit_diff - corrupted_logit_diff) / (
-                clean_logit_diff - corrupted_logit_diff
-            )
-
-            _ioi_patching_results.append(patched_result.item())
-            _ioi_patching_results.save()
-
-    ioi_patching_results.append(_ioi_patching_results)
-
-    
-
-
-    
-model.transformer.h[0].__sizeof__()
-
-## The difference 
-clean_logits[0].size() # SIZE: num_tokens, num of words 
-clean_hs[0].size()  # SIZE: layer, num_tokens, hidden state
-            
-            
-clean_logits[0].size()
-
-
-
-tokenized_prompt = model.tokenizer(clean_prompt)["input_ids"]
-print(model.tokenizer.decode(tokenized_prompt))
-
 N_LAYERS = len(model.transformer.h)
+clean_hs = []
+# Clean run
 
-ioi_patching_results = []
-with model.trace() as tracer:
-    # define barriers for each layer and token
-    barriers_per_layer = [tracer.barrier(len(tokenized_prompt)+1) for _ in range(N_LAYERS)]
-    # Clean run
-    with tracer.invoke(clean_prompt) as invoker:
-        clean_tokens = invoker.inputs[1]["input_ids"][0].save()
-
-        # Get hidden states of all layers in the network.
-        # We index the output at 0 because it's a tuple where the first index is the hidden state.
-        for layer_idx in range(N_LAYERS):
-            hidden_state = model.transformer.h[layer_idx].output[0]
-            # call barrier so nnsight can prepare the hidden states for the patching invoke
-            barriers_per_layer[layer_idx]()
-
-        # Get logits from the lm_head.
-        clean_logits = model.lm_head.output
-
-        # Calculate the difference between the correct answer and incorrect answer for the clean run and save it.
-        clean_logit_diff = (
+clean_token_id = model.tokenizer.encode(clean_prompt)
+with model.trace(clean_prompt, scan=True, validate=True) as tracer: 
+    for layer in range(len(model.transformer.h)):
+        out = model.transformer.h[layer].output[0].save()
+        clean_hs.append(out)
+    
+    clean_logits = model.lm_head.output
+    clean_logit_diff = (
             clean_logits[0, -1, correct_index] - clean_logits[0, -1, incorrect_index]
         ).save()
+    
+print(clean_logit_diff)
 
-    # Corrupted run
-    with tracer.invoke(corrupted_prompt) as invoker:
-        corrupted_logits = model.lm_head.output
+with model.trace(corrupted_prompt, scan=True, validate=True) as tracer: 
+    corrupted_logits = model.lm_head.output
+    
+    corrupted_logit_diff = (
+        corrupted_logits[0, -1, correct_index] - corrupted_logits[0, -1, incorrect_index]
+    ).save()
+    
+print(corrupted_logit_diff)
 
-        # Calculate the difference between the correct answer and incorrect answer for the corrupted run and save it.
-        corrupted_logit_diff = (
-            corrupted_logits[0, -1, correct_index]
-            - corrupted_logits[0, -1, incorrect_index]
-        ).save()
+ioi_patching_results = []
+for layer_idx in range(len(model.transformer.h)): 
+    _ioi_patching_results = [] 
+    for token_idx in range(len(clean_token_id)): 
+            with model.trace(corrupted_prompt, scan=True, validate=True) as tracer: 
 
-
-    # Activation Patching Intervention – across all layers & token positions
-    for layer_idx in range(len(model.transformer.h)):
-        _ioi_patching_results = []
-
-        for token_idx in range(len(tokenized_prompt)):
-            # Patching corrupted run at given layer and token
-
-            with tracer.invoke(corrupted_prompt) as invoker:
-                # call barrier so nnsight can grab the hidden states for this invoke
-                barriers_per_layer[layer_idx]()
-
-                # Patch in the clean hidden states over the corrupted hidden states.
-                model.transformer.h[layer_idx].output[0][:, token_idx, :] = hidden_state[:, token_idx, :]
-
+            # Setting the clean hidden state onto the bad hidden state 
+                model.transformer.h[layer_idx].output[0][:, token_idx] = clean_hs[layer_idx][:,token_idx,:]
                 patched_logits = model.lm_head.output
 
                 patched_logit_diff = (
                     patched_logits[0, -1, correct_index]
                     - patched_logits[0, -1, incorrect_index]
                 )
-
-                # Calculate the improvement in the correct token after patching.
+                
                 patched_result = (patched_logit_diff - corrupted_logit_diff) / (
                     clean_logit_diff - corrupted_logit_diff
                 )
 
-                _ioi_patching_results.append(patched_result.item())
+                _ioi_patching_results.append(patched_result.item().save())
 
-        ioi_patching_results.append(_ioi_patching_results)
-        ioi_patching_results.save()
-        
+            ioi_patching_results.append(_ioi_patching_results)
+
+print(ioi_patching_results)
+
