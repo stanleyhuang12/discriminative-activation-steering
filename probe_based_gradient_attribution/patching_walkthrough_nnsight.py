b6afbb0470255edb244fb80d3641078a712523c1
Stanley Huang <stanleyh201k@gmail.com>
2026-01-09 11:22:17 -0500

    patching walkthrough from nnsight

--- probe_based_gradient_attribution/patching_walkthrough_nnsight.py
+++ probe_based_gradient_attribution/patching_walkthrough_nnsight.py
@@ -1,12 +1,9 @@
-
 from nnsight import LanguageModel
-nnsight
-model = LanguageModel("openai-community/gpt2")
-
-model = LanguageModel("openai-community/gpt2", device_map=None)
-import nnsight
 from nnsight import CONFIG
+import torch
+import plotly.express as px
 
+model = LanguageModel("openai-community/gpt2")
 
 # This is an indirect object identification task 
 
@@ -59,9 +56,6 @@ GPT2LMHeadModel(
   (lm_head): Linear(in_features=768, out_features=50257, bias=False)
   (generator): WrapperModule()
 """
-N_LAYERS = len(model.transformer.h)
-clean_hs = []
-
 # Clean run
 N_LAYERS = len(model.transformer.h)
 clean_hs = []
@@ -103,14 +97,68 @@ for layer_idx in range(len(model.transformer.h)):
                     patched_logits[0, -1, correct_index]
                     - patched_logits[0, -1, incorrect_index]
                 )
-                
+
                 patched_result = (patched_logit_diff - corrupted_logit_diff) / (
                     clean_logit_diff - corrupted_logit_diff
                 )
 
                 _ioi_patching_results.append(patched_result.item().save())
+    ioi_patching_results.append(_ioi_patching_results)
 
-            ioi_patching_results.append(_ioi_patching_results)
 
-print(ioi_patching_results)
+clean_decoded_tokens = [model.tokenizer.decode(token) for token in clean_token_id]
+token_labels = [f"{token}_{index}" for index, token in enumerate(clean_decoded_tokens)]
 
+def plot_ioi_patching_results(res,
+                              x_labels,
+                              plot_title="Normalized Logit Difference After Patching Residual Stream on the IOI Task"):
+
+    fig = px.imshow(
+        res,
+        color_continuous_midpoint=0.0,
+        color_continuous_scale="RdBu",
+        labels={"x": "Position", "y": "Layer","color":"Norm. Logit Diff"},
+        x=x_labels,
+        title=plot_title,
+    )
+
+    return fig
+
+fig = plot_ioi_patching_results(ioi_patching_results,token_labels,"Patching GPT-2-small Residual Stream on IOI task")
+fig.show()
+
+N_LAYERS = len(model.transformer.h)
+
+
+tokenized_prompt = model.tokenizer(clean_prompt)["input_ids"]
+print(model.tokenizer.decode(tokenized_prompt))
+ioi_patching_results = []
+
+with model.trace() as tracer:
+    barriers_per_layer = [
+        tracer.barrier(len(tokenized_prompt) + 1)
+        for _ in range(N_LAYERS)
+    ]
+    clean_hidden_states = []
+    with tracer.invoke(clean_prompt) as invoker:
+        clean_tokens = invoker.inputs[1]["input_ids"][0].save()
+        for layer_idx in range(N_LAYERS):
+            hidden_state = model.transformer.h[layer_idx].output[0].save()
+            clean_hidden_states.append(hidden_state)
+            barriers_per_layer[layer_idx]()
+        clean_logits = model.lm_head.output
+        clean_logit_diff = (clean_logits[0, -1, correct_index]- clean_logits[0, -1, incorrect_index]).save()
+    with tracer.invoke(corrupted_prompt) as invoker:
+        corrupted_logits = model.lm_head.output
+        corrupted_logit_diff = (corrupted_logits[0, -1, correct_index] - corrupted_logits[0, -1, incorrect_index]).save()
+    for layer_idx in range(N_LAYERS):
+        _ioi_patching_results = []
+        for token_idx in range(len(tokenized_prompt)):
+            with tracer.invoke(corrupted_prompt) as invoker:
+                barriers_per_layer[layer_idx]()
+                model.transformer.h[layer_idx].output[0][:, token_idx, :] = (clean_hidden_states[layer_idx][:, token_idx, :])
+                patched_logits = model.lm_head.output
+                patched_logit_diff = (patched_logits[0, -1, correct_index] - patched_logits[0, -1, incorrect_index])
+                patched_result = ((patched_logit_diff - corrupted_logit_diff)/(clean_logit_diff - corrupted_logit_diff))
+                _ioi_patching_results.append(patched_result.item().save())
+        ioi_patching_results.append(_ioi_patching_results)
